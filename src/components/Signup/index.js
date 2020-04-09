5610ee773533b4b5ffa73d85e9239b699a67dc85
Rony Santiago <santy.usuga@gmail.com>
2020-04-09 15:30:35 -0500

    Sign up created

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Logo from '../../static/logo-login.png'
 
 export const Signup = () =>  (
-    <h1>
-        Esto es un Signup
-    </h1>
+    <div className="login">
+        <form>
+            <img src={Logo} />
+            <h3>Sign Up</h3>
+            <input type="radio" name="gender" value="Male"></input>
+            <input type="radio" name="gender" value="Female"></input>
+            <div className="input-custom">
+                <label>Name </label>
+                <input type="email"  />
+            </div>
+            <div className="input-custom">
+                <label>Username</label>
+                <input type="password" />
+            </div>
+            <div className="input-custom">
+                <label>Email</label>
+                <input type="password" />
+            </div>
+            <div className="input-custom">
+                <label>Cellphone</label>
+                <input type="password" />
+
+            </div>
+            <br />
+            <div className="buttom-primary">
+                <input type="button" value="Sign Up"/>
+            </div>
+        </form>
+    </div>
 );
